9aebfc8108a060708e0108468281fec6d995293e
Michael Linhardt <linhardt.michael@gmail.com>
2022-05-23 16:16:05 +0700

    test

---
+++ scripts/installs/vsc_extensions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+code --install-extension sashaweiss.block-travel
+code --install-extension chunsen.bracket-select
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension zainchen.json
+code --install-extension pkief.material-icon-theme
+code --install-extension leizongmin.node-module-intellisense
+code --install-extension eg2.vscode-npm-script
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension christian-kohler.path-intellisense
+code --install-extension visualstudioexptteam.vscodeintellicode
+# vscInstallExtension "ms-vsliveshare.vsliveshare"
