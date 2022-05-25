d5311f80d91964737de6eb1455d9e91b0c6cdd3f
Michael Linhardt <linhardt.michael@gmail.com>
2022-05-25 10:32:36 +0700

    fix alias and add one for headless beauty

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -5,13 +5,16 @@ dkt () { cd ~/dkt/$@ }
 
 # GIT
 gad () { git add $@ }
-gst () { sh ~/scripts/git/gstatus.sh }
-gco () { sh ~/scripts/git/gcommit.sh }
-gpu () { sh ~/scripts/git/gpush.sh }
-gacp () { sh ~/scripts/git/gaddcompush.sh }
+alias gst="sh ~/scripts/git/gstatus.sh"
+alias gco="sh ~/scripts/git/gcommit.sh"
+alias gpu="sh ~/scripts/git/gpush.sh"
+alias gacp="sh ~/scripts/git/gaddcompush.sh"
 
-gdelete () { sh ~/scripts/git/gdelete.sh }
-gcreate () { sh ~/scripts/git/gcreate.sh }
-gbranch () { git remote prune origin && git branch -a }
-grebase () { git fetch && git rebase && git pull }
-gsub () { git submodule update --init }
+alias gdelete="sh ~/scripts/git/gdelete.sh"
+alias gcreate="sh ~/scripts/git/gcreate.sh"
+alias gbranch="git remote prune origin && git branch -a"
+alias grebase="git fetch && git rebase && git pull"
+alias gsub="git submodule update --init"
+
+# DKT WORK
+start_healdless_beauty () { cd ~/dkt/headless-beauty-international && nvm use 12.22.10 && npm run dev }
